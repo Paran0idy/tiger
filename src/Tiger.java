57b7b7568ec50a825b56d988db01fd1c7f593c84
hbj <bjhua@ustc.edu.cn>
2024-04-17 12:05:26 +0800

    lab1

--- src/Tiger.java
+++ src/Tiger.java
@@ -1,36 +1,28 @@
 import control.CommandLine;
 import parser.Parser;
 
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
-import java.io.InputStream;
-
 // the Tiger compiler main class.
 public class Tiger {
     public static void main(String[] args) throws Exception {
-        InputStream fileStream;
         Parser parser;
 
         // ///////////////////////////////////////////////////////
-        // handle command line arguments
+        // process command line arguments
         CommandLine cmd = new CommandLine();
-        // the file to be compiled:
+        // get the file to be compiled
         String fileName = cmd.scan(args);
         if (fileName == null) {
-            // no file is given, then exit silently.
+            // no input file is given, then exit silently.
             return;
         }
 
         // /////////////////////////////////////////////////////////
-        // otherwise, we continue the normal compilation phases.
-        // first, create a new stream from the input file:
-        fileStream = new BufferedInputStream(new FileInputStream(fileName));
-        // then create a parser:
-        parser = new Parser(fileName, fileStream);
-        // parse the file using the parser:
+        // otherwise, we continue the normal compilation pipeline.
+        // first, create a parser:
+        parser = new Parser(fileName);
+        // then use it to parse the input file:
         parser.parse();
-        // close the stream before exiting.
-        fileStream.close();
+
     }
 }
 
