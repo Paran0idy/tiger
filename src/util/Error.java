7462f609f1fa9260a31a67a449b2a68a74121cdc
hbj <bjhua@ustc.edu.cn>
2024-04-21 22:23:33 +0800

    lab1

--- src/util/Error.java
+++ src/util/Error.java
@@ -1,13 +1,16 @@
 package util;
 
 public class Error extends AssertionError {
+    private final String foregroundColor = "\033[31;4m";
+    private final String backgroundColor = "\033[0m";
+
     public Error() {
         super();
-        System.out.println("Compiler error");
+        System.out.println(STR."\n\{this.foregroundColor}Compiler error\{backgroundColor}");
     }
 
     public Error(Object obj) {
         super(obj);
-        System.out.println("Compiler error");
+        System.out.println(STR."\n\{this.foregroundColor}Compiler error\{backgroundColor}");
     }
 }
